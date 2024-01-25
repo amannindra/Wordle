39d8d91209d2477270cddf13fdf57d943fe9a91c
Aman Nindra <amannindra@gmail.com>
2024-01-24 22:45:17 -0800

    cahadsfsd

--- wordle.py
+++ wordle.py
@@ -4,51 +4,21 @@ from datetime import datetime
 import string
 
 
-def get_words(length):
-    random_number = random.randint(1, 4671)
-    number = 1
-    with open('words.txt') as file:
-        file_content = file.read()
-        for word in file_content.split():
-            if len(word) == length:
-                if (number == random_number):
-                    word_list = word
-                    break
-                else:
-                    number += 1
-    return word_list
-
-
-def words(numbers):
-    number = 0
-    with open('words.txt') as file:
-        file_content = file.read()
-        for word in file_content.split():
-            if len(word) == 6:
-                number += 1
-    return number
-
-
 class Wordle:
     def __init__(self):
         self.word = None
         self.matrix = []
-        for i in range(self.wordLength):
-            row = []
-            for j in range(self.wordLength):
-                row.append("_")
-            self.matrix.append(row)
         self.user_input = None
         self.trys = 0
         self.check = True
-        self.wordLength = None
+        self.wordLength = 5
 
     def getUserInput(self):
-        self.user_input = input("Give me a 5 letter word: ")
+        self.user_input = input("Give me a " + str(self.wordLength) + " letter word: ")
 
-    def drawTryMatrix(self, user_input):
+    def drawTryMatrix(self, user_input, word):
         print(self.user_input, self.word)
-        for i in range(5):
+        for i in range(self.wordLength):
             num = 0
             if self.user_input[i] == self.word[i]:
                 self.matrix[self.trys][i] = self.user_input[i]
@@ -57,25 +27,74 @@ class Wordle:
             else:
                 self.matrix[self.trys][i] = "_"
                 num = 0
-        if(num == 5):
+        if (num == 5):
             self.check = False
+    def wordLength1(self):
+        while True:
+            self.wordLength = input("Word Length: ")
+            if self.wordLength.isdigit():
+                self.wordLength = int(self.wordLength)
+                if 0 < self.wordLength < 12:
+                    break
+                else: 
+                    print("Please enter a word length from 1 to 12.")
+            else:
+                print("Please enter a valid integer.")
+
     def Start(self):
-        
-        self.wordLength = input("Word Length: ")
-        self.word = get_words(self.wordLength)
+        self.wordLength1()
+        self.word = self.get_words(self.wordLength)
+        print(f"Check Now: {self.word}")
+        for i in range(self.wordLength):
+            row = []
+            for j in range(5):
+                row.append("_")
+            self.matrix.append(row)
         while (self.check):
             self.getUserInput()
-            if(len(self.getUserInput) != self.wordLength):
+            if (len(self.user_input) != self.wordLength):
                 self.getUserInput()
             else:
-                self.drawTryMatrix(self.user_input)
+                print("Before: " + str(self.user_input) + ", " + str(self.word))
+                self.drawTryMatrix()
                 self.printMatrix()
                 self.trys += 1
-                if(self.check == False):
+                if (self.check == False):
                     print("Nice Job! You have completed the Wordle in " + str(self.trys))
-              
+
     def printMatrix(self):
         for r in self.matrix:
-            print(r)   
+            print(r)
+
+    def get_words(self, length):
+        random_number = random.randint(1, 4671)
+        number = 1
+        word_list = ""
+        with open('words.txt') as file:
+            file_content = file.read()
+            for word in file_content.split():
+                if len(word) == length:
+                    if (number == random_number):
+                        word_list = word
+                        break
+                    else:
+                        number += 1
+        if word_list is None:
+            print(f"No word found with length {length}")
+            return None
+        else:
+            print(f"There is a word found with length {length}")
+            self.word = word_list
+'''
+    def words(self, numbers):
+        number = 0
+        with open('words.txt') as file:
+            file_content = file.read()
+            for word in file_content.split():
+                if len(word) == 6:
+                    number += 1
+        return number
+
+'''
 word = Wordle()
 word.Start()
