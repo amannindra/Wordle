b3d9d9e2ef42e2360d8c2a3323900f66165a90bf
Aman Nindra <amannindra@gmail.com>
2024-01-26 21:39:25 -0800

    done

--- wordle.py
+++ wordle.py
@@ -1,7 +1,10 @@
 
+
 import random
 from datetime import datetime
 import string
+
+
 
 
 class Wordle:
@@ -13,11 +16,13 @@ class Wordle:
         self.check = True
         self.wordLength = 5
 
+
     def getUserInput(self):
         self.user_input = input("Give me a " + str(self.wordLength) + " letter word: ")
 
-    def drawTryMatrix(self, user_input, word):
-        print(self.user_input, self.word)
+
+    def drawTryMatrix(self):
+        print(f"Debug: {self.user_input}, {self.word}")
         for i in range(self.wordLength):
             num = 0
             if self.user_input[i] == self.word[i]:
@@ -36,18 +41,19 @@ class Wordle:
                 self.wordLength = int(self.wordLength)
                 if 0 < self.wordLength < 12:
                     break
-                else: 
+                else:
                     print("Please enter a word length from 1 to 12.")
             else:
                 print("Please enter a valid integer.")
 
+
     def Start(self):
         self.wordLength1()
-        self.word = self.get_words(self.wordLength)
+        self.get_words(self.wordLength)
         print(f"Check Now: {self.word}")
         for i in range(self.wordLength):
             row = []
-            for j in range(5):
+            for j in range(self.wordLength):
                 row.append("_")
             self.matrix.append(row)
         while (self.check):
@@ -59,13 +65,22 @@ class Wordle:
                 self.drawTryMatrix()
                 self.printMatrix()
                 self.trys += 1
-                if (self.check == False):
-                    print("Nice Job! You have completed the Wordle in " + str(self.trys))
+                self.checkfullword()
+               
+
 
     def printMatrix(self):
         for r in self.matrix:
             print(r)
 
+
+    def checkfullword(self):
+        for i in range(self.wordLength):
+            if(self.user_input[i] != self.word[i])
+                break
+            else:
+                self.check = False
+                print("Contrats you got the Word in " + str(self.trys))
     def get_words(self, length):
         random_number = random.randint(1, 4671)
         number = 1
@@ -83,7 +98,6 @@ class Wordle:
             print(f"No word found with length {length}")
             return None
         else:
-            print(f"There is a word found with length {length}")
             self.word = word_list
 '''
     def words(self, numbers):
@@ -95,6 +109,12 @@ class Wordle:
                     number += 1
         return number
 
+
 '''
 word = Wordle()
 word.Start()
+
+
+
+
+
